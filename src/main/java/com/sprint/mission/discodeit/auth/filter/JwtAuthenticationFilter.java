c249c282b15bc8c8901959a9f684510b28d25245
B1uffer <135810601+B1uffer@users.noreply.github.com>
2025-10-23 11:30:39 +0900

    JWT 인증 필터 구현 추가합니다.

--- src/main/java/com/sprint/mission/discodeit/auth/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/sprint/mission/discodeit/auth/filter/JwtAuthenticationFilter.java
@@ -1,20 +1,49 @@
 package com.sprint.mission.discodeit.auth.filter;
 
+import com.sprint.mission.discodeit.auth.jwt.JwtTokenProvider;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    // 요청당 한번만 실행, OncePerRequestFilter 상속
+    // 요청당 한번만 실행되도록 OncePerRequestFilter 상속
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserDetailsService userDetailsService;
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider, UserDetailsService userDetailsService) {
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.userDetailsService = userDetailsService;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
+        String token = request.getHeader("Authorization");
+        if(!token.startsWith("Bearer ")) { // Bearer 이 포함된 토큰만 인증함
+            throw new ServletException("올바른 토큰 형식이 아닙니다.");
+        } else {
+            Map<String, Object> claims = jwtTokenProvider.getClaims(token); // JwtProvider를 통해 엑세스 토큰 유효성 검사
+            UserDetails details = userDetailsService.loadUserByUsername(claims.get("username").toString()); // 밑에 details
 
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    details,
+                    null,
+                    details.getAuthorities()
+            );
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
     }
+
 }
